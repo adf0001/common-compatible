069db24a0622f7653e0dd63909d8bd9311574e87
fwg <adf0001@163.com>
2018-09-05 17:35:53 +0800

    add bind()

--- _test/1.watchify-all.bat
+++ _test/1.watchify-all.bat
@@ -5,6 +5,7 @@ set watchifyPath="C:\Users\Administrator\AppData\Roaming\npm\watchify.cmd"
 
 %watchifyPath% -o ./all.bundle.js -v ^
 	-r ../dom/addEventListener.js:addEventListener ^
+	-r ../js/function/bind.js:bind ^
 
 
 pause
--- js/function/bind.js
+++ js/function/bind.js
@@ -1,14 +1,4 @@
-require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"addEventListener":[function(require,module,exports){
-
-//addEventListener()
-module.exports= function( thisObject, type, listener /*, options*/ ){
-	if( thisObject.addEventListener ) return thisObject.addEventListener( thisObject, Array.prototype.slice.call(arguments,1) );
-	else if( thisObject.attachEvent ) return thisObject.attachEvent.apply( thisObject, ["on"+type].concat(Array.prototype.slice.call(arguments,2)) );
-	else thisObject["on"+type]= listener;
-};
-
-},{}],"bind":[function(require,module,exports){
-
+﻿
 
 //bind()
 module.exports= function( func /*, bindThisObject ,...*/ ){
@@ -45,5 +35,3 @@ module.exports= function( func /*, bindThisObject ,...*/ ){
 	return func.bind.apply(func,Array.prototype.slice.call(arguments,1));
 };
 
-
-},{}]},{},[]);
