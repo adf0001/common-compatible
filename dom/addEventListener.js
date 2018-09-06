ddedd92a95335189cc7d0fe818c4b30ef978005a
fwg <adf0001@163.com>
2018-09-06 20:58:31 +0800

    fix bug

--- dom/addEventListener.js
+++ dom/addEventListener.js
@@ -1,7 +1,7 @@
 
 //addEventListener()
 module.exports= function( thisObject, type, listener /*, options*/ ){
-	if( thisObject.addEventListener ) return thisObject.addEventListener( thisObject, Array.prototype.slice.call(arguments,1) );
+	if( thisObject.addEventListener ) return thisObject.addEventListener.apply( thisObject, Array.prototype.slice.call(arguments,1) );
 	else if( thisObject.attachEvent ) return thisObject.attachEvent.apply( thisObject, ["on"+type].concat(Array.prototype.slice.call(arguments,2)) );
 	else thisObject["on"+type]= listener;
 };
